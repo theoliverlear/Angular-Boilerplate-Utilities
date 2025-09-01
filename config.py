d7affe5e4facf95c572eba21054a7be635fb9596
theoliverlear <sigwartholiver2@gmail.com>
2025-09-01 10:04:05 -0500

    Refactor config.py to allow for project modules to act as source directories to better find Angular files

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 import os
 
 def detect_source_directory(selected_directory):
-    absolute_path = os.path.abspath(selected_directory)
-    path_parts = absolute_path.split(os.sep)
-    if "GitHub" in path_parts:
-        github_index = path_parts.index("GitHub")
-        if github_index + 1 < len(path_parts):
-            source_directory = os.sep.join(path_parts[:github_index + 2])
-            print(f"Detected source directory: {source_directory}")
-            return source_directory
-    print("Error: Could not detect source directory. Ensure the selected path"
-          " includes a 'GitHub' folder.")
+    current_directory = selected_directory
+    while True:
+        if "angular" in os.listdir(current_directory):
+            return os.path.abspath(current_directory)
+        parent_directory = os.path.dirname(current_directory)
+        if current_directory == parent_directory:
+            # Return the absolute path
+            return os.path.abspath(current_directory)
+        current_directory = parent_directory
+        if "GitHub" in os.listdir(current_directory):
+            return None
     return None
 
